667e7ea83c7f4ad1fcce7fcbf39685fa82850e99
Laurent St-Hilaire <202154572@edu.clg.qc.ca>
2023-03-18 18:06:41 -0400

    test

--- Assets/Scripts/AI/NavigationBaker.cs
+++ Assets/Scripts/AI/NavigationBaker.cs
@@ -8,37 +8,25 @@ using Unity.AI.Navigation;
 public class NavigationBaker : MonoBehaviour
 {
 
-    public NavMeshSurface[] surfaces;
-    public Transform[] objectsToRotate;
-    [SerializeField] GameObject RoadHelper;
-
+    public NavMeshSurface[] surfaces; 
     // Use this for initialization
     void Start()
     {
         StartCoroutine(TrouverSurface());
-    }
-    IEnumerator TrouverSurface()
-    {
-        yield return new WaitForSeconds(1);
-        int nbRues = RoadHelper.transform.childCount;
-        surfaces = new NavMeshSurface[nbRues];
-        for (int i = 0; i < nbRues; i++)
-        {
-            surfaces[i] = RoadHelper.transform.GetChild(i).GetComponent<NavMeshSurface>();
-        }
-
-    }
-    void Update()
-    {
-        for (int j = 0; j < objectsToRotate.Length; j++)
-        {
-            objectsToRotate[j].localRotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
-        }
-
         for (int i = 0; i < surfaces.Length; i++)
         {
             surfaces[i].BuildNavMesh();
         }
+    }
+    IEnumerator TrouverSurface()
+    {
+        yield return new WaitForSeconds(1);
+        
+
+    }
+    void Update()
+    {
+       
     }
 
 
