229ea4f4e2c2cb9ccbde47b367c348b993be0e05
Laurent St-Hilaire <202154572@edu.clg.qc.ca>
2023-04-12 17:05:07 -0400

    Auto modifié vr generation joueur

--- Assets/Scripts/Joueur/GénérationJoueur.cs
+++ Assets/Scripts/Joueur/GénérationJoueur.cs
@@ -3,36 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GénérationJoueur : MonoBehaviour
-{   
-    Camera playerHead;
-    [SerializeField]Transform CaméraEmplacement;
-    GameObject player;
-    int vieJoueur = 100;
-    private void Awake()
-    {
-        player = gameObject;
-        playerHead = Camera.main;
-    }
+{
+    [SerializeField] Transform CaméraEmplacement;
+    GameObject Joueur;
 
-    public void AssocierCamera()
+    public void AssocierCamera(GameObject XRorigin)
     {
-        float rotationAngleY = playerHead.transform.rotation.eulerAngles.y - CaméraEmplacement.rotation.eulerAngles.y;
-        player.transform.Rotate(0, rotationAngleY, 0);
-        var distanceDiff = CaméraEmplacement.position - playerHead.transform.position;
-        player.transform.position += distanceDiff;
-    }
-    public void VérifierVieJoueur()
-    {
-        if (vieJoueur == 0)
-        {
-
-        }
+        Joueur = XRorigin;
+        float rotationAngleY = Joueur.transform.rotation.eulerAngles.y - CaméraEmplacement.rotation.eulerAngles.y;
+        Joueur.transform.Rotate(0, rotationAngleY, 0);
+        var distanceDiff = CaméraEmplacement.position - Joueur.transform.position;
+        Joueur.transform.position += distanceDiff;
     }
     // Update is called once per frame
     void Update()
     {
-        playerHead.transform.position = CaméraEmplacement.transform.position;
-        playerHead.transform.rotation = CaméraEmplacement.transform.rotation;
+        Joueur.transform.position = CaméraEmplacement.transform.position;
+        Joueur.transform.rotation = CaméraEmplacement.transform.rotation;
     }
-   
+
 }
