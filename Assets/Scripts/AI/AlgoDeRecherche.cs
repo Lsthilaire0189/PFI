622bb744e706e2d58938c2fa5bc251ec89b9efe8
Laurent St-Hilaire <202154572@edu.clg.qc.ca>
2023-04-19 11:24:28 -0400

    GestionJoueur.cs

---
+++ Assets/Scripts/AI/AlgoDeRecherche.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+class Noeud
+{
+    public GameObject Route { get; set; }
+    public Noeud Suivant { get; set; }
+    public Noeud Précédent { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+    public bool visité { get; set; }
+    public int coût { get; set; }
+
+    public Noeud(GameObject route)
+    {
+        Route = route;
+        Suivant = null; // Pas nécessaire, mais plus clair
+        Précédent = null; // Pas nécessaire, mais plus clair
+        x = (int)route.transform.position.x + 50;
+        y = (int)route.transform.position.z + 50;
+    }
+}
+
+public class AlgoDeRecherche : MonoBehaviour
+{
+    public GameObject[,] Carte;
+    public List<Vector3> chemin;
+    public List<Vector3> AlgoDijkstra(GameObject[,] carte, GameObject départ, GameObject destination)
+    {
+        Carte = carte;
+        Queue<Noeud> frontière = new Queue<Noeud>();
+        Noeud Départ = new Noeud(départ);
+        Départ.coût = 0;
+        Noeud Destination = new Noeud(destination);
+        Noeud current;
+        List<GameObject> NoeudVisités = new List<GameObject>();
+        frontière.Enqueue(Départ);
+        while (frontière.Count > 0)
+        {
+            current = frontière.Dequeue();
+            NoeudVisités.Add(current.Route);
+            if (current.Route.transform.position == Destination.Route.transform.position)
+            {
+                Destination.Précédent = current.Précédent;
+                break;
+            }
+            var voisin = TrouverVoisins(current);
+            for (int i = 0; i < voisin.Count; i++)
+            {
+                if (!NoeudVisités.Contains(voisin[i].Route))
+                {
+                    voisin[i].Précédent = current;
+                    frontière.Enqueue(voisin[i]);
+                }
+            }
+
+        }
+        ConstructionChemin(Destination);
+        return chemin;
+
+    }
+    void ConstructionChemin(Noeud Destination)
+    {
+        Stack<Noeud> temp = new Stack<Noeud>();
+        Noeud current = Destination;
+        temp.Push(current);
+        chemin = new List<Vector3>();
+        while (current.Précédent != null)
+        {
+            temp.Push(current.Précédent);
+            current = current.Précédent;
+        }
+        while (temp.Count > 0)
+        {
+            chemin.Add(temp.Pop().Route.transform.position);
+        }
+    }
+    List<Noeud> TrouverVoisins(Noeud current)
+    {
+        List<Noeud> voisin = new List<Noeud>();
+        if (Carte[current.x - 1, current.y] != null)
+        {
+            voisin.Add(new Noeud(Carte[current.x - 1, current.y]));
+
+        }
+        if (Carte[current.x + 1, current.y] != null)
+        {
+            voisin.Add(new Noeud(Carte[current.x + 1, current.y]));
+
+        }
+        if (Carte[current.x, current.y - 1] != null)
+        {
+            voisin.Add(new Noeud(Carte[current.x, current.y - 1]));
+
+        }
+        if (Carte[current.x, current.y + 1] != null)
+        {
+
+            voisin.Add(new Noeud(Carte[current.x, current.y + 1]));
+
+        }
+        return voisin;
+    }
+
+
+}
---
+++ Assets/Scripts/Joueur/GestionJoueur.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class GestionJoueur : MonoBehaviour
+{
+    [SerializeField] Transform CaméraEmplacement;
+    GameObject Joueur;
+    DéplacementScript déplacementScript;
+    EssenceManager essenceManager;
+    CollisionOutilsManager collisionOutilsManager;
+
+    public void Awake()
+    {
+        déplacementScript = gameObject.GetComponent<DéplacementScript>();
+        essenceManager = gameObject.GetComponent<EssenceManager>();
+        collisionOutilsManager = gameObject.GetComponent<CollisionOutilsManager>(); 
+    }
+    public void InitierSpécifications(int UpgradeAccélération, int UpgradeVie, int UpgradeEssence, int UpgradeVitesseMaximale)
+    {
+        déplacementScript.ValeurAccélération += UpgradeAccélération;
+        déplacementScript.VitesseMaximum += UpgradeVitesseMaximale;
+    }
+
+    public void AssocierCamera(GameObject XRorigin)
+    {
+        Joueur = XRorigin;
+        float rotationAngleY = Joueur.transform.rotation.eulerAngles.y - CaméraEmplacement.rotation.eulerAngles.y;
+        Joueur.transform.Rotate(0, rotationAngleY, 0);
+        var distanceDiff = CaméraEmplacement.position - Joueur.transform.position;
+        Joueur.transform.position += distanceDiff;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Joueur.transform.position = CaméraEmplacement.transform.position;
+        Joueur.transform.rotation = CaméraEmplacement.transform.rotation;
+
+    }
+    void VérifierJoueurPeutAvancer()
+    {
+
+    }
+}
