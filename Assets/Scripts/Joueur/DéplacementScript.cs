25a838a5ede7375175ba0fae86d8a4864ffaf110
Laurent St-Hilaire <202154572@edu.clg.qc.ca>
2023-04-24 10:04:16 -0400

    reset

--- Assets/Scripts/Joueur/DéplacementScript.cs
+++ Assets/Scripts/Joueur/DéplacementScript.cs
@@ -70,66 +70,66 @@ public class DéplacementScript : MonoBehaviour
         goRoueAvantGauche.transform.Rotate(vRotation,Angle,0);
         goRoueArrGauche.transform.Rotate(vRotation,0,0);
         goRoueArrDroite.transform.Rotate(-vRotation,0,0);
-                
-                
-        //if (gestionEssence.VérifierEssence())
-        //{
-        //    if (rec.lY is < 32760 and > 0)
-        //    {
-        //        Accélération =
-        //            direction*Mathf.Log(ValeurAccélération * (32760f - rec.lY) / 32760f + 1,
-        //                2);
-
-        //        RoueArrièreDroite.motorTorque += Accélération;
-        //        RoueArrièreGauche.motorTorque += Accélération;
-        //        RoueAvantDroite.motorTorque += Accélération;
-        //        RoueAvantGauche.motorTorque += Accélération;
-        //        //print("case 1 true");
-
-        //    }
-        //    else if (rec.lY < 0)
-        //    {
-        //        Accélération = direction*Mathf.Log(ValeurAccélération * (-rec.lY + 32760) / 32760f + 1, 2);
 
 
-        //        RoueArrièreDroite.motorTorque += Accélération;
-        //        RoueArrièreGauche.motorTorque += Accélération;
-        //        RoueAvantDroite.motorTorque += Accélération;
-        //        RoueAvantGauche.motorTorque += Accélération;
-        //        //print("case 2 true");
-        //    }
-                
-            
-                
-        //    if (rec.lRz is < 32760 and > 0 && RoueArrièreDroite.motorTorque > 0)
-        //    {
-        //        ForceFreinage = ValeurForceFreinage * (32760-rec.lRz)/32760f;
-        //        RoueAvantDroite.motorTorque -= ForceFreinage;
-        //        RoueAvantGauche.motorTorque -= ForceFreinage;
-        //        RoueArrièreDroite.motorTorque -= ForceFreinage;
-        //        RoueArrièreGauche.motorTorque -= ForceFreinage;
-        //        //print("case 3 true");
-        //    }
-        //    else if (rec.lRz < 0 && RoueArrièreDroite.motorTorque > 0)
-        //    {
-        //        ForceFreinage = ValeurForceFreinage * (rec.lY + 32760)/32760f;
-        //        RoueAvantDroite.motorTorque -= ForceFreinage;
-        //        RoueAvantGauche.motorTorque -= ForceFreinage;
-        //        RoueArrièreDroite.motorTorque -= ForceFreinage;
-        //        RoueArrièreGauche.motorTorque -= ForceFreinage;
-        //    } 
-            
-            
-        //    if (RoueArrièreDroite.motorTorque > 0 && RoueArrièreGauche.motorTorque > 0 &&
-        //          RoueAvantDroite.motorTorque > 0 && RoueAvantGauche.motorTorque > 0 && rec.lY>32760 && rec.lRz>32760)
-        //    {
-        //        RoueArrièreDroite.motorTorque -= 0.03f;
-        //        RoueArrièreGauche.motorTorque -= 0.03f;
-        //        RoueAvantDroite.motorTorque -= 0.03f;
-        //        RoueAvantGauche.motorTorque -= 0.03f;
-        //        //print("case 4 true");
-        //    }
-            
+        if (gestionEssence.VérifierEssence())
+        {
+            if (rec.lY is < 32760 and > 0)
+            {
+                Accélération =
+                    direction * Mathf.Log(ValeurAccélération * (32760f - rec.lY) / 32760f + 1,
+                        2);
+
+                RoueArrièreDroite.motorTorque += Accélération;
+                RoueArrièreGauche.motorTorque += Accélération;
+                RoueAvantDroite.motorTorque += Accélération;
+                RoueAvantGauche.motorTorque += Accélération;
+                //print("case 1 true");
+
+            }
+            else if (rec.lY < 0)
+            {
+                Accélération = direction * Mathf.Log(ValeurAccélération * (-rec.lY + 32760) / 32760f + 1, 2);
+
+
+                RoueArrièreDroite.motorTorque += Accélération;
+                RoueArrièreGauche.motorTorque += Accélération;
+                RoueAvantDroite.motorTorque += Accélération;
+                RoueAvantGauche.motorTorque += Accélération;
+                //print("case 2 true");
+            }
+
+
+
+            if (rec.lRz is < 32760 and > 0 && RoueArrièreDroite.motorTorque > 0)
+            {
+                ForceFreinage = ValeurForceFreinage * (32760 - rec.lRz) / 32760f;
+                RoueAvantDroite.motorTorque -= ForceFreinage;
+                RoueAvantGauche.motorTorque -= ForceFreinage;
+                RoueArrièreDroite.motorTorque -= ForceFreinage;
+                RoueArrièreGauche.motorTorque -= ForceFreinage;
+                //print("case 3 true");
+            }
+            else if (rec.lRz < 0 && RoueArrièreDroite.motorTorque > 0)
+            {
+                ForceFreinage = ValeurForceFreinage * (rec.lY + 32760) / 32760f;
+                RoueAvantDroite.motorTorque -= ForceFreinage;
+                RoueAvantGauche.motorTorque -= ForceFreinage;
+                RoueArrièreDroite.motorTorque -= ForceFreinage;
+                RoueArrièreGauche.motorTorque -= ForceFreinage;
+            }
+
+
+            if (RoueArrièreDroite.motorTorque > 0 && RoueArrièreGauche.motorTorque > 0 &&
+                  RoueAvantDroite.motorTorque > 0 && RoueAvantGauche.motorTorque > 0 && rec.lY > 32760 && rec.lRz > 32760)
+            {
+                RoueArrièreDroite.motorTorque -= 0.03f;
+                RoueArrièreGauche.motorTorque -= 0.03f;
+                RoueAvantDroite.motorTorque -= 0.03f;
+                RoueAvantGauche.motorTorque -= 0.03f;
+                //print("case 4 true");
+            }
+
         }
         else
         {
