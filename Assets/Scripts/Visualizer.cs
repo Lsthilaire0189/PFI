df4318c5cdd2241006fb93ff3ee0255ecc4e67f9
Nicholas Spektor <202136443@edu.clg.qc.ca>
2023-04-24 10:42:08 -0400

    bonne version visualizer

--- Assets/Scripts/Visualizer.cs
+++ Assets/Scripts/Visualizer.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 
 public class Visualizer : MonoBehaviour
 {
-  
-    public GameObject Arbre;
+    Circuit circuit;
+    public GameObject Tree;
     public LSystemGenerator lsystem;
     List<Vector3> positions = new List<Vector3>();
-    CircuitCarte circuit;
 
     public RoadHelper roadHelper;
-        public StructureHelper structureHelper;
 
-        public ArgentSpawner argentInitiateur;
-        
-        private int length = 6;
-        private float angle = 90;
+    public StructureHelper structureHelper;
+
+    public EnvironnementScript instancierEnvironnement;
 
     private int length = 6;
     private float angle = 90;
@@ -42,7 +39,7 @@ public class Visualizer : MonoBehaviour
     {
         var sequence = lsystem.GenerateSentence();
         VisualizeSequence(sequence);
-        
+        circuit = gameObject.AddComponent<Circuit>();
     }
 
     private void VisualizeSequence(string sequence)
@@ -52,7 +49,7 @@ public class Visualizer : MonoBehaviour
 
         Vector3 direction = Vector3.forward;
         Vector3 tempPosition = Vector3.zero;
-            
+
         positions.Add(currentPosition);
 
         foreach (var letter in sequence)
@@ -60,61 +57,54 @@ public class Visualizer : MonoBehaviour
             SimpleVisualizer.EncodingLetters encoding = (SimpleVisualizer.EncodingLetters)letter;
             switch (encoding)
             {
-                SimpleVisualizer.EncodingLetters encoding = (SimpleVisualizer.EncodingLetters)letter;
-                switch (encoding)
-                {
-                    case SimpleVisualizer.EncodingLetters.save:
-                        savePoints.Push(new AgentParameters
-                        {
-                            position = currentPosition,
-                            direction = direction,
-                            length = Length
-                        });
-                        break;
-                    case SimpleVisualizer.EncodingLetters.load:
-                        if (savePoints.Count > 0)
-                        {
-                            var agentParameter = savePoints.Pop();
-                            currentPosition = agentParameter.position;
-                            direction = agentParameter.direction;
-                            Length = agentParameter.length;
-                        }
-                        else
-                        {
-                            throw new System.Exception("Don't have saved point in our stack");
-                        }
-                        break;
-                    case SimpleVisualizer.EncodingLetters.draw:
-                        tempPosition = currentPosition;
-                        currentPosition += direction * length;
-                        roadHelper.PlaceStreetPositions(tempPosition, Vector3Int.RoundToInt(direction), length);
-                        Length -= 2;
-                        positions.Add(currentPosition);
-                        break;
-                    case SimpleVisualizer.EncodingLetters.turnRight:
-                        direction = Quaternion.AngleAxis(angle, Vector3.up) * direction;
-                        break;
-                    case SimpleVisualizer.EncodingLetters.turnLeft:
-                        direction = Quaternion.AngleAxis(-angle, Vector3.up) * direction;
-                        break;
-                    default:
-                        break;
-                }
+                case SimpleVisualizer.EncodingLetters.save:
+                    savePoints.Push(new AgentParameters
+                    {
+                        position = currentPosition,
+                        direction = direction,
+                        length = Length
+                    });
+                    break;
+                case SimpleVisualizer.EncodingLetters.load:
+                    if (savePoints.Count > 0)
+                    {
+                        var agentParameter = savePoints.Pop();
+                        currentPosition = agentParameter.position;
+                        direction = agentParameter.direction;
+                        Length = agentParameter.length;
+                    }
+                    else
+                    {
+                        throw new System.Exception("Dont have saved point in our stack");
+                    }
+                    break;
+                case SimpleVisualizer.EncodingLetters.draw:
+                    tempPosition = currentPosition;
+                    currentPosition += direction * length;
+                    roadHelper.PlaceStreetPositions(tempPosition, Vector3Int.RoundToInt(direction), length);
+                    Length -= 2;
+                    positions.Add(currentPosition);
+                    break;
+                case SimpleVisualizer.EncodingLetters.turnRight:
+                    direction = Quaternion.AngleAxis(angle, Vector3.up) * direction;
+                    break;
+                case SimpleVisualizer.EncodingLetters.turnLeft:
+                    direction = Quaternion.AngleAxis(-angle, Vector3.up) * direction;
+                    break;
+                default:
+                    break;
             }
         }
-            environnementScript.InstancierEnvironnement();
-            argentInitiateur.InstatierMonnaies();
-            
-            var spline = new Circuit(); 
 
         roadHelper.FixRoad();
-        circuit = new CircuitCarte(Arbre,roadHelper);
-
+        //var listv = new List<Vector3>() { new Vector3(0, 0, 21), new Vector3(1, 0, 24), new Vector3(2, 0, 24), new Vector3(3, 0, 18), new Vector3(4, 0, 16) };
+        //var listv = ci
+        // circuit = new Circuit(listv,Tree);
+        //circuit.InstancierObjets();
+        instancierEnvironnement.InstancierEnvironnement();
         structureHelper.PlaceStructureAroundRoad(roadHelper.GetRoadPositions());
-        
 
 
     }
-   
 }
 
