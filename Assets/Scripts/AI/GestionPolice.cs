f512abdf2d7c6bb18ba8ec4ef14b801dd9abb12d
Laurent St-Hilaire <202154572@edu.clg.qc.ca>
2023-04-26 19:58:12 -0400

    Ai

--- Assets/Scripts/AI/GestionPolice.cs
+++ Assets/Scripts/AI/GestionPolice.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 public class GestionPolice : MonoBehaviour
 {
     Ray ray;
-    float maxDistance = 100;
-    public LayerMask layer;
+    float maxDistance = 2;
+    public LayerMask layer = 14;
+    Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
-        ray=new Ray(transform.position, transform.forward);       
     }
     void CheckForColliders()
     {
-        if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, layer))
+        if (Physics.Raycast(ray, out RaycastHit hit,maxDistance))
         {
-            Vector3.MoveTowards(transform.position, hit.transform.position,20f);
+            if (hit.collider.gameObject.layer == 14)
+            {
+                print(hit.collider.gameObject.name);
+                position = hit.collider.transform.position;
+            }
+
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (position== null)
+        {
+            ray = new Ray(transform.position, transform.forward);
         CheckForColliders();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, position, 0.005f);
+        }
+
     }
 }
